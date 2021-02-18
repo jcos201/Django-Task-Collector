55510c26a3245f925ac3a9923f643d386be3465e
Jorge L. Cossi <jcos201@gmail.com>
2021-02-18 07:57:20 -0500

    Adds url & path for Home and About

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Task Collector Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Task Collector</h1>')
