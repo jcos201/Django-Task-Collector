39b834e7d4e1239f5c9546b74ee0ee15a6fee6ef
Jorge L. Cossi <jcos201@gmail.com>
2021-02-20 18:32:01 -0500

    Add Tasks to views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -10,4 +12,5 @@ def about(request):
     return render(request, 'about.html')
 
 def tasks_index(request):
+    tasks = Task.objects.all()
     return render(request, 'tasks/index.html', { 'tasks': tasks })
