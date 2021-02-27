7215d63b979a6aaf8d8d453977312e8002350e48
Jorge L. Cossi <jcos201@gmail.com>
2021-02-27 00:39:07 -0500

    prepares for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taskcollector.wsgi
--- taskcollector/settings.py
+++ taskcollector/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/tasks/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
