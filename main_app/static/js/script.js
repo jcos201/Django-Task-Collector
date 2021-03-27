2336370da2e7351fa8af9c909f548dc2a423e020
Jorge L. Cossi <jcos201@gmail.com>
2021-03-27 06:53:24 -0400

    Adds font stylings

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+
+    setInterval(function(){
+      $('.carousel').carousel('next');
+    }, 4000)
+  });
